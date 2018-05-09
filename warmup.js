97d435f24aa868d139bbb539069b1be9c758c0e7
donrestarone <shashikejayatunge@gmail.com>
2018-05-09 16:23:37 -0400

    completed counting warmup

--- warmup.js
+++ warmup.js
@@ -15,9 +15,30 @@ console.log(words)
 var num = 2;
 
 if (num > 10) {
-	console.log(num + ' is greater than 10')
+	console.log(num + ' is greater than 10');
 } else if (num === 10) {
-	console.log(num + ' is exactly 10')
+	console.log(num + ' is exactly 10');
 } else {
-	console.log(num + ' must be less than 10')
+	console.log(num + ' must be less than 10');
+}
+
+
+for (i = 0; i < 10; i++) {
+	console.log('doing the same thing over and over');
+}
+
+var base = 5;
+
+for (i = 0; i < 20; i++) {
+	console.log(i + base);
+}
+
+
+
+
+
+for (i = 0; i < 100; i++) {
+	var total = 0 + 1;
+	total += i
+	console.log(total);
 }
