300201ddba8e615a5d70a0af88d9dc9f4fcbcd54
donrestarone <shashikejayatunge@gmail.com>
2018-05-09 16:08:31 -0400

    complete if elsif and else warmup

--- warmup.js
+++ warmup.js
@@ -1,3 +1,23 @@
 var apples = 14;
 console.log(apples);
 console.log('I have ' + apples + ' apples');
+
+var materials = ['wood', 'metal', 'stone'];
+var words = {
+	'elephant': "world's largest land mammal",
+	'school': 'a place of learning',
+	'ice cream': 'a delicious milk-based dessert',
+};
+
+console.log(materials)
+console.log(words)
+
+var num = 2;
+
+if (num > 10) {
+	console.log(num + ' is greater than 10')
+} else if (num === 10) {
+	console.log(num + ' is exactly 10')
+} else {
+	console.log(num + ' must be less than 10')
+}
